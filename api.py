91911d04dc412348424faaae536f23bb65354e72
Vincent <vincentz92@nycstudents.net>
2025-11-18 10:16:14 -0500

    Update api.py

--- api.py
+++ api.py
@@ -1,9 +1,16 @@
 import tkinter as tk
+def dates():
 window = tk.Tk()
-window.title("Timeline Creator") # title at the top of the window
-window.geometry("1200x750") # set the size (width x height)
-window.resizable(False, False) # keep it from being resized
-event = tk.Label(window, text = "Type an event, followed by a date.")
+window.title("Timeline Creator")
+window.geometry("1200x750")
+window.resizable(False, False)
+event = tk.Label(window, text = "Type an event")
 font = ("Arial", 14)
 event.pack(pady = 10)
+event.pack(pady = 10)
+entry = tk.Entry(window, font = ("Arial", 14), width = 30)
+entry.pack(pady=5)
+sort_button = tk.Button(window, text = "Sort Events!",
+font = ("Arial", 14), command = dates)
+sort_button.pack(pady=10)
 window.mainloop()
