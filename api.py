1ab9f7d31600773d70fb4fbdb39e5049ca4a3c30
Vincent <vincentz92@nycstudents.net>
2025-11-19 10:16:27 -0500

    Update api.py

--- api.py
+++ api.py
@@ -1,16 +1,22 @@
 import tkinter as tk
-def dates():
+def forward():
+    text = entry.get()
+def backward():
+    text = entry.get()
 window = tk.Tk()
-window.title("Timeline Creator")
+window.title("Cipher")
 window.geometry("1200x750")
 window.resizable(False, False)
-event = tk.Label(window, text = "Type an event")
+prompt = tk.Label(window, text = "Type something to encode")
 font = ("Arial", 14)
-event.pack(pady = 10)
-event.pack(pady = 10)
+prompt.pack(pady = 10)
+prompt.pack(pady = 10)
 entry = tk.Entry(window, font = ("Arial", 14), width = 30)
 entry.pack(pady=5)
-sort_button = tk.Button(window, text = "Sort Events!",
-font = ("Arial", 14), command = dates)
-sort_button.pack(pady=10)
+move_forward = tk.Button(window, text = "Shift forward",
+font = ("Arial", 14), command = forward)
+move_forward.pack(pady = 10)
+move_backward = tk.Button(window, text = "Shift backward",
+font = ("Arial", 14), command = forward)
+move_backward.pack(side = tk.LEFT, padx = 10)
 window.mainloop()
