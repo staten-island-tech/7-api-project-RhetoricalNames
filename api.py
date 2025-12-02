469afdfe02fa14d96a33c5ef470a05cf20c229bd
Vincent <vincentz92@nycstudents.net>
2025-12-02 10:13:18 -0500

    Update api.py

--- api.py
+++ api.py
@@ -1,22 +1,27 @@
 import tkinter as tk
 import requests
+
+#cd app
+#Scripts\Activate.ps1
  
 def search(input):
     comic = requests.get(f"https://xkcd.com/{input}/info.0.json")
     transcript = comic.json()
-    print(dict(transcript)['year'])
+    print(dict(transcript))
 
 window = tk.Tk()
 window.title("API")
 window.geometry("1200x750")
 window.resizable(False, False)
-input = tk.Label(window, text = "Type a comic number")
-font = ("Arial", 14)
-input.pack(pady = 10)
-searchbox = tk.Entry(window, font = ("Arial", 14), width = 30)
+
+instructions = tk.Label(window, text = "Type a comic number")
+instructions.pack(pady = 10)
+
+number = tk.IntVar()
+searchbox = tk.Entry(window, font = ("Arial", 14), width = 30, textvariable=number)
 searchbox.pack(pady = 5)
-search = tk.Button(window, text = "Search",
-font = ("Arial", 14), command = search(input))
-search.pack(pady = 10)
+
+search_button = tk.Button(window, text = "Search",
+font = ("Arial", 14), command = lambda: search(number.get()))
+search_button.pack(pady = 10)
 window.mainloop()
-search(1)
