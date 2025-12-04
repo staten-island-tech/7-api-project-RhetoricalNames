e5ab6527950ec5be5cc91acb2f8ef1e8ab5cbdcf
Vincent <vincentz92@nycstudents.net>
2025-12-04 10:01:39 -0500

    Update api.py

--- api.py
+++ api.py
@@ -16,16 +16,16 @@ number = tk.IntVar()
 searchbox = tk.Entry(window, font = ("Arial", 14), width = 30, textvariable=number)
 searchbox.pack(pady = 5)
 
-transcript = tk.Label(window, text="")
+Toutput = tk.Label(window, text="", pady= 10, fg="black", font="Arial, 11", wraplength=500)
 
 def search():
     search = searchbox.get()
     comic = requests.get(f"https://xkcd.com/{search}/info.0.json")
     transcript = comic.json()
     print(transcript)
-    transcript.config(text=transcript)
+    Toutput.config(text=transcript)
 
 search_button = tk.Button(window, text = "Search",
-font = ("Arial", 14), command = search)
+font = ("Arial", 14), bg= "SteelBlue", fg= "White", command = search)
 search_button.pack(pady = 10)
 window.mainloop()
