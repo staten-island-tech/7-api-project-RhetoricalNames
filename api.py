da0b2c262a81d8c327a8ff32c8dc108513830045
Vincent <vincentz92@nycstudents.net>
2025-12-08 10:16:45 -0500

    Update api.py

--- api.py
+++ api.py
@@ -3,17 +3,17 @@ import requests
 
 #cd app
 #Scripts\Activate.ps1
-
+""" 
 window = tk.Tk()
-window.title("API")
+window.title("Chess API")
 window.geometry("1200x750")
 window.resizable(False, False)
 
-instructions = tk.Label(window, text = "Type a comic number")
+instructions = tk.Label(window, text = "Type a username")
 instructions.pack(pady = 10)
 
-number = tk.IntVar()
-searchbox = tk.Entry(window, font = ("Arial", 14), width = 30, textvariable=number)
+username = tk.StringVar()
+searchbox = tk.Entry(window, font = ("Arial", 14), width = 30, textvariable=username)
 searchbox.pack(pady = 5)
 
 Uoutput = tk.Label(window, text="", fg="black", font=("Times New Roman", 14), wraplength=500)
@@ -21,12 +21,27 @@ Uoutput.pack(pady=20)
 
 def search():
     Uinput = searchbox.get()
-    comic = requests.get(f"https://api.chess.com/pub/player/{Uinput}")
-    transcript = comic.json()
-    print(transcript)
-    Uoutput.config(text=transcript, bg= "SteelBlue")
+    user = requests.get(f"https://api.chess.com/pub/player/{Uinput}")
+    print(user)
+    stats = user.json()
+    print(stats)
+    Uoutput.config(text=stats, bg= "SteelBlue")
 
 search_button = tk.Button(window, text = "Search",
 font = ("Arial", 14), bg= "SteelBlue", fg= "White", command = search)
 search_button.pack(pady = 10)
-window.mainloop()
+window.mainloop() """
+def search(a):
+    Uinput = a
+    user = requests.get(f"https://api.chess.com/pub/player/{Uinput}")
+    print(user) #response 403
+"""     stats = user.json()
+    print(stats) """
+search("erik")
+def b(a):
+    titleabbrev = a
+    user = requests.get(f"https://api.chess.com/pub/titled/{titleabbrev}")
+    print(user) #response 403
+"""     stats = user.json()
+    print(stats) """
+b("erik")
