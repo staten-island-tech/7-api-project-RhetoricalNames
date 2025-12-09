7dec6887df2ba8e9c44d312e1349a3999145c648
Vincent <vincentz92@nycstudents.net>
2025-12-09 10:17:14 -0500

    Update api.py

--- api.py
+++ api.py
@@ -3,45 +3,32 @@ import requests
 
 #cd app
 #Scripts\Activate.ps1
-""" 
+
 window = tk.Tk()
-window.title("Chess API")
+window.title("API")
 window.geometry("1200x750")
 window.resizable(False, False)
 
-instructions = tk.Label(window, text = "Type a username")
+instructions = tk.Label(window, text = "Type a number")
 instructions.pack(pady = 10)
 
-username = tk.StringVar()
+username = tk.IntVar()
 searchbox = tk.Entry(window, font = ("Arial", 14), width = 30, textvariable=username)
 searchbox.pack(pady = 5)
 
-Uoutput = tk.Label(window, text="", fg="black", font=("Times New Roman", 14), wraplength=500)
-Uoutput.pack(pady=20)
+Toutput = tk.Label(window, text="", fg="black", font=("Times New Roman", 14), wraplength=500)
+Toutput.pack(pady=20)
+
+Ioutput = tk.Label(window, image=) #image
 
 def search():
-    Uinput = searchbox.get()
-    user = requests.get(f"https://api.chess.com/pub/player/{Uinput}")
-    print(user)
+    input = searchbox.get()
+    user = requests.get(f"https://xkcd.com/{input}/info.0.json")
     stats = user.json()
-    print(stats)
-    Uoutput.config(text=stats, bg= "SteelBlue")
+    Toutput.config(text=stats, bg= "SteelBlue")
+    stats['img']
 
 search_button = tk.Button(window, text = "Search",
 font = ("Arial", 14), bg= "SteelBlue", fg= "White", command = search)
 search_button.pack(pady = 10)
-window.mainloop() """
-def search(a):
-    Uinput = a
-    user = requests.get(f"https://api.chess.com/pub/player/{Uinput}")
-    print(user) #response 403
-"""     stats = user.json()
-    print(stats) """
-search("erik")
-def b(a):
-    titleabbrev = a
-    user = requests.get(f"https://api.chess.com/pub/titled/{titleabbrev}")
-    print(user) #response 403
-"""     stats = user.json()
-    print(stats) """
-b("erik")
+window.mainloop()
