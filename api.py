4fdf9997c791b9e9ed4943040e794593375b729f
Vincent <vincentz92@nycstudents.net>
2025-12-11 10:00:46 -0500

    Update api.py

--- api.py
+++ api.py
@@ -1,5 +1,7 @@
 import tkinter as tk
 import requests
+from PIL import Image, ImageTk
+from io import BytesIO
 
 #cd app
 #Scripts\Activate.ps1
@@ -19,15 +21,22 @@ searchbox.pack(pady = 5)
 Toutput = tk.Label(window, text="", fg="black", font=("Times New Roman", 14), wraplength=500)
 Toutput.pack(pady=20)
 
-Ioutput = tk.PhotoImage()
-
 def search():
     input = searchbox.get()
     user = requests.get(f"https://xkcd.com/{input}/info.0.json")
     stats = user.json()
     Toutput.config(text=stats, bg= "SteelBlue")
-    Ioutput.config(file=requests.get(dict(stats)['img']))
-    print(dict(stats)['img'])
+
+    response = requests.get(dict(stats)['img'])
+    img_data = response.content
+    print(img_data)
+    pillow_image = Image.open(BytesIO(img_data))
+    print(pillow_image)
+    tk_image = ImageTk.PhotoImage(pillow_image)
+    print(tk_image)
+    Ioutput = tk.Label(image = tk_image)
+    Ioutput.pack()
+
 
 
 search_button = tk.Button(window, text = "Search",
