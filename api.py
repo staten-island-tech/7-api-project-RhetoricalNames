f04a408724293eb818d27a39b774af9f9d91dddb
Vincent <vincentz92@nycstudents.net>
2025-12-15 10:17:09 -0500

    Update api.py

--- api.py
+++ api.py
@@ -11,7 +11,7 @@ window.title("API")
 window.geometry("1200x750")
 window.resizable(False, False)
 
-instructions = tk.Label(window, text = "Type a number", font= ("Arial", 18))
+instructions = tk.Label(window, text = "Type a number", font= ("Arial", 16))
 instructions.pack(pady = 10)
 
 username = tk.IntVar()
@@ -37,18 +37,20 @@ def search_image():
     input = searchbox.get()
     user = requests.get(f"https://xkcd.com/{input}/info.0.json")
     stats = user.json()
-    response = requests.get(dict(stats)['img'])
-    img_data = response.content
-    pillow_image = Image.open(BytesIO(img_data))
+    response = requests.get(stats['img'])
+    response.raise_for_status()
+    pillow_image = Image.open(BytesIO(response.content))
     tk_image = ImageTk.PhotoImage(pillow_image)
+    tk_image.thumbnail((300, 200), Image.LANCZOS)
     Ioutput = tk.Label(image = tk_image)
+    Ioutput.image = tk_image
     Ioutput.pack()
 
 
-search_button = tk.Button(window, text="Search for transcript", font = ("Times New Roman", 14), bg= "SteelBlue", fg= "White", command = search_transcript)
+search_button = tk.Button(window, text="Search for transcript", font = ("Arial", 14), bg= "SteelBlue", fg= "White", command = search_transcript)
 search_button.pack(pady=10)
-image_button = tk.Button(window, text="Search for image", font = ("Times New Roman", 14), bg= "SteelBlue", fg= "White", command = search_image)
+image_button = tk.Button(window, text="Search for image", font = ("Arial", 14), bg= "SteelBlue", fg= "White", command = search_image)
 image_button.pack(pady=20)
-stats_button = tk.Button(window, text="Other information", font = ("Times New Roman", 14), bg= "SteelBlue", fg= "White", command = other_stats)
+stats_button = tk.Button(window, text="Other information", font = ("Arial", 14), bg= "SteelBlue", fg= "White", command = other_stats)
 stats_button.pack(pady=20)
-window.mainloop()
+window.mainloop() 
