5d77ea115ce678818880771f6ceeaf5a2b19eed3
Vincent <vincentz92@nycstudents.net>
2025-12-22 10:16:25 -0500

    Update api.py

--- api.py
+++ api.py
@@ -2,6 +2,7 @@ import tkinter as tk
 import requests
 from PIL import Image, ImageTk
 from io import BytesIO
+import random
 
 #cd app
 #Scripts\Activate.ps1
@@ -21,8 +22,6 @@ searchbox.pack(pady = 5)
 output = tk.Label(window, text="", fg="black", font=("Times New Roman", 14), wraplength=500)
 output.pack(pady=20)
 
-def validate_input():
-
 def find_statistics():
     gametitle = userinput.get()
     info = requests.get("https://www.freetogame.com/api/games")
@@ -35,9 +34,18 @@ def find_statistics():
     output.config(text="Output not found. Please try again.")
 
 def performance():
-    user = requests.get(f"https://www.freetogame.com/api/games/{userinput}")
-    games_stats = user.json()
-    output.config(text=f"{games_stats["minimum_system_requirements"]}")
+    gametitle = userinput.get()
+    info = requests.get("https://www.freetogame.com/api/games")
+    games_stats = info.json()
+    for game in games_stats:
+        if game["title"].lower() == gametitle.lower():
+            game_info = requests.get(f"https://www.freetogame.com/api/game?id={game['id']}")
+            converted_data = game_info.json()
+            output.config(
+                text=converted_data["minimum_system_requirements"]
+            )
+            return
+    output.config(text="Output not found. Please try again.")
 
 def images():
     user = requests.get(f"https://www.freetogame.com/api/games/{userinput}")
@@ -51,11 +59,16 @@ def images():
     Ioutput.image = tk_image
     Ioutput.pack()
 
+def random_search():
+    
+
 
 search_button = tk.Button(window, text="Search via name", font = ("Arial", 14), bg= "SteelBlue", fg= "White", command = find_statistics)
 search_button.pack(pady=10)
 image_button = tk.Button(window, text="Screenshots", font = ("Arial", 14), bg= "SteelBlue", fg= "White", command = images)
-image_button.pack(pady=20)
+image_button.pack(pady=10)
 stats_button = tk.Button(window, text="Performance information", font = ("Arial", 14), bg= "SteelBlue", fg= "White", command = performance)
-stats_button.pack(pady=20)
+stats_button.pack(pady=10)
+random_search_button = tk.Button(window, text="Random game", font = ("Arial", 14), bg= "SteelBlue", fg= "White", command = random_search)
+stats_button.pack(pady=10)
 window.mainloop() 
