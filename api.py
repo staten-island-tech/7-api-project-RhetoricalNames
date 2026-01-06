efd1a369b1ea5d83f3f200be10c097564a3428b0
Vincent <vincentz92@nycstudents.net>
2026-01-05 20:29:49 -0500

    Update api.py

--- api.py
+++ api.py
@@ -12,68 +12,59 @@ window.title("F2P game discovery")
 window.geometry("1200x750")
 window.resizable(False, False)
 
-instructions = tk.Label(window, text = "Type the name of a game", font= ("Arial", 15))
+instructions = tk.Label(window, text = "Select a random game, than find more about it!", font= ("Arial", 15))
 instructions.pack(pady = 10)
 
-userinput = tk.StringVar()
-searchbox = tk.Entry(window, font = ("Arial", 14), width = 20, textvariable=userinput)
-searchbox.pack(pady = 5)
-
+title_output = tk.Label(window, text="", font= ("Times New Roman", 12))
+title_output.pack(pady=5)
 output = tk.Label(window, text="", fg="black", font=("Times New Roman", 14), wraplength=500)
 output.pack(pady=20)
-
-def find_statistics():
-    gametitle = userinput.get()
-    info = requests.get("https://www.freetogame.com/api/games")
-    games_stats = info.json()
-    for game in games_stats:
-        if game["title"].lower() == gametitle.lower():
-            output.config(
-                text=f"Title: {game['title']}")
-            return
-    output.config(text="Output not found. Please try again.")
-
-def performance():
-    gametitle = userinput.get()
-    info = requests.get("https://www.freetogame.com/api/games")
-    games_stats = info.json()
-    for game in games_stats:
-        if game["title"].lower() == gametitle.lower():
-            game_info = requests.get(f"https://www.freetogame.com/api/game?id={game['id']}")
-            converted_data = game_info.json()
-            output.config(
-                text=converted_data["minimum_system_requirements"]
-            )
-            return
-    output.config(text="Output not found. Please try again.")
-
-def images():
-    user = requests.get(f"https://www.freetogame.com/api/games/{userinput}")
-    image = user.json()
-    response = requests.get(image["screenshots"][0])
-    response.raise_for_status()
-    pillow_image = Image.open(BytesIO(response.content))
-    tk_image = ImageTk.PhotoImage(pillow_image)
-    tk_image.thumbnail((300, 200), Image.LANCZOS)
-    Ioutput = tk.Label(image = tk_image)
-    Ioutput.image = tk_image
-    Ioutput.pack()
+Ioutput = tk.Label(image="")
+Ioutput.pack(pady=10)
+game_data = ""
 
 def random_search():
+    Ioutput.config(image="")
     data = requests.get("https://www.freetogame.com/api/games")
     random_number = random.randint(1, len(data.json()))
     data = data.json()
+    global game_data
     game_data = requests.get(f"https://www.freetogame.com/api/game?id={data[random_number]["id"]}")
-    output.config(text=f"'{game_data["title"]}' Short description:")
-    
+    game_data = game_data.json()
+    title_output.config(text=f"Selected game: {game_data["title"]}")
 
 
-search_button = tk.Button(window, text="Search via name", font = ("Arial", 14), bg= "SteelBlue", fg= "White", command = find_statistics)
-search_button.pack(pady=10)
-image_button = tk.Button(window, text="Screenshots", font = ("Arial", 14), bg= "SteelBlue", fg= "White", command = images)
+
+def performance():
+    if game_data == "":
+        output.config(text="Game not selected!")
+        return
+    output.config(text=f"System requirements:{game_data["minimum_system_requirements"]}")
+
+def thumbnail():
+    if game_data == "":
+        output.config(text="Game not selected!")
+        return
+    thumbnail_link = game_data["thumbnail"]
+    response = requests.get(thumbnail_link)
+    pillow_image = Image.open(BytesIO(response.content))
+    pillow_image.thumbnail((300, 200), Image.LANCZOS)
+    tk_image = ImageTk.PhotoImage(pillow_image)
+    Ioutput.config(image=tk_image)
+    Ioutput.image = tk_image
+
+def info(): 
+    if game_data == "":
+        output.config(text="Game not selected!")
+        return
+    output.config(text=f"Developer: {game_data["developer"]}\nPublisher: {game_data["publisher"]}\nShort description: '{game_data["short_description"]}'\ngenre: '{game_data["genre"]}'\ndeveloper: '{game_data["developer"]}'\nGame link: {game_data["game_url"]}")
+
+random_search_button = tk.Button(window, text="Random game", font = ("Arial", 14), bg= "SteelBlue", fg= "White", command = random_search)
+random_search_button.pack(pady=10)
+image_button = tk.Button(window, text="Search for thumbnail", font = ("Arial", 14), bg= "SteelBlue", fg= "White", command = thumbnail)
 image_button.pack(pady=10)
 stats_button = tk.Button(window, text="Performance information", font = ("Arial", 14), bg= "SteelBlue", fg= "White", command = performance)
 stats_button.pack(pady=10)
-random_search_button = tk.Button(window, text="Random game", font = ("Arial", 14), bg= "SteelBlue", fg= "White", command = random_search)
-random_search_button.pack(pady=20)
+info_button = tk.Button(window, text="Game information", font = ("Arial", 14), bg= "SteelBlue", fg= "White", command = info)
+info_button.pack(pady=10)
 window.mainloop() 
